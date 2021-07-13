4ff87fd25f51d2ed656c7f304eb41c2688aab3be
rhelmstedter <rhelmstedter@gmail.com>
2021-07-12 17:12:57 -0700

    updated .gitignore

--- venv/lib/python3.9/site-packages/neovim/api/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Nvim API subpackage.
-
-This is a transition package. New projects should instead import pynvim.api.
-"""
-from pynvim import api
-from pynvim.api import *
-
-__all__ = api.__all__
--- venv/lib/python3.9/site-packages/pynvim/plugin/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Nvim plugin/host subpackage."""
-
-from pynvim.plugin.decorators import (autocmd, command, decode, encoding, function,
-                                      plugin, rpc_export, shutdown_hook)
-from pynvim.plugin.host import Host
-
-
-__all__ = ('Host', 'plugin', 'rpc_export', 'command', 'autocmd',
-           'function', 'encoding', 'decode', 'shutdown_hook')
